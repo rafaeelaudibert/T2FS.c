b177a471ce14a2533375cf7d6370d4ab5528faaf
RafaAudibert <rafaeelaudibert@gmail.com>
2019-11-16 18:31:59 -0300

    :memo: Added t2fslib.h documentation

--- include/t2fslib.h
+++ include/t2fslib.h
@@ -12,40 +12,144 @@ typedef struct t2fs_superbloco SUPERBLOCK;
 typedef struct t2fs_record RECORD;
 typedef struct t2fs_inode I_NODE;
 
-// "Private" functions
-void initialize();
-int readMBR();
-int formatPartition(int, int);
-DWORD computeChecksum(SUPERBLOCK *);
-int createRootFolder(int);
-int configureMountedPartition(int);
-int unmountPartition();
-void openRoot();
-void closeRoot();
-BOOL finishedEntries(I_NODE *);
-DWORD getBlockBitmapFirstSector(PARTITION *, SUPERBLOCK *);
-DWORD getBlockBitmapLastSector(PARTITION *, SUPERBLOCK *);
-DWORD getInodeBitmapFirstSector(PARTITION *, SUPERBLOCK *);
-DWORD getInodeBitmapLastSector(PARTITION *, SUPERBLOCK *);
-DWORD getInodesFirstSector(PARTITION *, SUPERBLOCK *);
-DWORD getDataBlocksFirstSector(PARTITION *, SUPERBLOCK *);
-BOOL notMountedPartition();
-BOOL notRootOpened();
-MBR *getMBR();
-SUPERBLOCK *getSuperblock();
-PARTITION *getPartition();
-int getBlocksize();
-int readDataBlockSector(int, int, I_NODE *, BYTE *);
-BYTE *getBuffer(size_t);
-BYTE *getZeroedBuffer(size_t);
-I_NODE *getInode(DWORD);
-int getCurrentDirectoryEntryIndex();
-void nextDirectoryEntry();
-int getRecordByNumber(int, RECORD *);
+/*
 
-// iNodePointersQuantities
+    FUNCTIONS USED TO INITIALIZE THE LIBRARY
+
+*/
+
+// Initialize the needed structures
+void initialize();
+
+/*
+
+    FUNCTIONS USED ON FORMAT2
+
+*/
+// Once called, reads the disk MBR to memory
+int readMBR();
+
+//  Formats a given partition `partition_number` with each logical block
+//  occupying `sectors_per_block` physical sectors
+int formatPartition(int partition_number, int sectors_per_block);
+
+// Compute the Checksum for a given `superblock`
+DWORD computeChecksum(SUPERBLOCK *superblock);
+
+//  Initializes the needed structures to hold the root folder
+//  for a given partition identified by `partition_number`
+int createRootFolder(int partition_number);
+
+/*
+
+    FUNCTIONS USED ON MOUNT2
+
+*/
+// Mount the partition identified by the number `partition_number` on "\\",
+// reading their superblock to the memory
+int configureMountedPartition(int partition_number);
+
+// Unmount the partition currently mounted on "\\" path
+int unmountPartition();
+
+/*
+
+    FUNCTIONS USED ON OPENDIR2
+
+*/
+// Open the folder located at "\\" initializing the used variables
+void openRoot();
+
+/*
+
+    FUNCTIONS USED ON CLOSEDIR2
+
+*/
+// Close the folder located at "\\" freeing the used variables
+void closeRoot();
+
+/*
+
+    FUNCTIONS USED ON READDIR2
+
+*/
+// Verifies if all the entries on the directory have already been read
+BOOL finishedEntries(I_NODE *inode);
+
+// Returns the current directory entry, which is constantly incremented in the `readdir2` function
+// by calling `nextDirectoryEntry`
+int getCurrentDirectoryEntryIndex();
+
+// Increments the current `directoryEntryIndex`
+void nextDirectoryEntry();
+
+/*
+
+    GENERIC FUNCTIONS
+
+*/
+// Returns the number of the block where the Block Bitmap starts
+DWORD getBlockBitmapFirstSector(PARTITION *, SUPERBLOCK *);
+
+// Returns the number of the block where the Block Bitmap ends
+DWORD getBlockBitmapLastSector(PARTITION *, SUPERBLOCK *);
+
+// Returns the number of the block where the Inode Bitmap starts
+DWORD getInodeBitmapFirstSector(PARTITION *, SUPERBLOCK *);
+
+// Returns the number of the block where the Inode Bitmap ends
+DWORD getInodeBitmapLastSector(PARTITION *, SUPERBLOCK *);
+
+// Returns the number of the block where the Inodes start
+DWORD getInodesFirstSector(PARTITION *, SUPERBLOCK *);
+
+// Returns the number of the block where the Data blocks start
+DWORD getDataBlocksFirstSector(PARTITION *, SUPERBLOCK *);
+
+// Checks if a partition is already mounted
+BOOL notMountedPartition();
+
+// Checks if the root folder "\\" is already opened
+BOOL notRootOpened();
+
+// Returns a `MBR` pointer to the disk MBR
+MBR *getMBR();
+
+// Returns a `SUPERBLOCK` pointer to the superblock
+// of the currently mounted partition
+SUPERBLOCK *getSuperblock();
+
+// Returns a `PARTITION` pointer to the currently
+// mounted partition
+PARTITION *getPartition();
+
+// Returns the size of the block in bytes
+int getBlocksize();
+
+// Reads the sector `sector_number` from the block `block_number` from a file
+// identified by the inode `inode`.
+// The sector information is copied to the `buffer` pointer.
+int readDataBlockSector(int block_number, int sector_number, I_NODE *inode, BYTE *buffer);
+
+// Returns a newly allocated buffer, with size `size` (similar to malloc)
+BYTE *getBuffer(size_t size);
+
+// Returns a newly allocated buffer, with their content zeroed, with size `size` (similar to calloc)
+BYTE *getZeroedBuffer(size_t size);
+
+// Returns the inode of number `inodeNumber` in the inode blocks
+I_NODE *getInode(DWORD inodeNumber);
+
+// Gets a record by its number, filling the `record` structure
+int getRecordByNumber(int number, RECORD *structure);
+
+// Quantity of direct blocks that an INODE can hold
 DWORD getInodeDirectQuantity();
+
+// Quantity of simple indirection block that an INODE can hold
 DWORD getInodeSimpleIndirectQuantity();
+
+// Quantity of double indirection blocks that an INODE can hold
 DWORD getInodeDoubleIndirectQuantity();
 
 #endif
